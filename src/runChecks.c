cd586a8b94a1515996c6b2b4e7274f17f281daa3
Noah Arcouette <kcu9626@gmail.com>
2022-07-19 20:15:00 -0600

    add library path check

---
+++ src/runChecks.c
@@ -0,0 +1,97 @@
+#include "structures.h"
+#include "functions.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <strings.h>
+#include <dirent.h>
+#include <msap.h>
+#include <errno.h>
+#include <sys/stat.h>
+
+int checkDirs (sa s)
+{
+	DIR* dir;
+	for (register size_t i = 0; i<s.size; i++)
+	{
+		dir = opendir(s.items[i]);
+
+		if (dir)
+		{
+			printf("\x1b[32mExists \x1b[39m      ┃ Directory \x1b[32m%s\n", s.items[i]);
+
+			closedir(dir);
+		}
+		else if (ENOENT == errno)
+		{
+			mkdir(s.items[i], S_IRWXU);
+
+			printf("\x1b[33mCreated \x1b[39m     ┃ Directory \x1b[32m%s\n", s.items[i]);
+		}
+		else
+		{
+			printf("\x1b[31mError \x1b[39m       ┃ Directory \x1b[32m%s\n", s.items[i]);
+
+			return 1;
+		}
+	}
+
+	return 0;
+}
+
+int runChecks (struct hd_settings *s)
+{
+	printf("\x1b[1m");
+
+	// Source path
+	printf("\x1b[35mSource Path \x1b[39m ┃\n");
+
+	if (s->srcDirs.size < 1)
+	{
+		saPush(&s->srcDirs, "./src/");
+		printf("\x1b[33mWARNING      \x1b[39m┃ Setting default I.E \x1b[32m./src/\n");
+	}
+
+	if (checkDirs(s->srcDirs))
+	{
+		return 1;
+	}
+
+	// Include path
+	printf("\x1b[39m             ┃\n\x1b[35mInclude Path\x1b[39m ┃\n");
+
+	if (s->incDirs.size < 1)
+	{
+		saPush(&s->incDirs, "./inc/");
+		printf("\x1b[33mWARNING      \x1b[39m┃ Setting default I.E \x1b[32m./inc/\n");
+	}
+
+	if (checkDirs(s->incDirs))
+	{
+		return 1;
+	}
+
+	// Library Path
+	printf("\x1b[39m             ┃\n\x1b[35mLibrary Path\x1b[39m ┃\n");
+
+	if (s->libDirs.size < 1)
+	{
+		printf("\x1b[32mDISCLUDED    \x1b[39m┃ No library path\n");
+	}
+	else if (checkDirs(s->libDirs))
+	{
+		return 1;
+	}
+
+	return 0;
+}
+
+/*
+struct hd_settings
+{
+	char *binDir; // check if exist, if not then create, or default
+	char *objDir; // check if exist, if not then create, or default
+
+	char *name;    // no check, or default
+	char *version; // check if valid version, or default
+};
+*/
